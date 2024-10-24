c6fe6ca843c92d83ca5566c9a391061b432b4a9d
Favas Muhammed <favassakkeer13@gmail.com>
2024-10-24 02:23:22 +0200

    commit 9

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,10 +1,15 @@
-exports.getAllPosts = async (req, res) => {
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+router.delete("/:id", async (req, res) => {
   try {
-    const posts = await Post.find()
-      .sort({ createdAt: -1 })
-      .populate("user", "username");
-    res.json(posts);
+    const post = await Post.findByIdAndRemove(req.params.id);
+    res.json(post);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Error deleting post" });
   }
-};
+});
+
+module.exports = router;
